30c3b0bb61dde23750236580c4e4fc46bf4003b5
Abdullah <59245838+athamov@users.noreply.github.com>
2023-01-06 14:59:50 +0500

    files uploaded

---
+++ maket/js/index.js
@@ -0,0 +1,15 @@
+// window.onscroll = function() {myFunction()};
+
+// const header = document.getElementById("header");
+// const main = document.getElementById("main");
+// const sticky = header.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset + 30 > sticky) {
+//     header.classList.add("sticky");
+//     main.classList.add("extra-margin")
+//   } else {
+//     header.classList.remove("sticky");
+//     main.classList.remove("extra-margin")
+//   }
+// }
